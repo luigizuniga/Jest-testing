b2e8b90074ede95fe66358dd948aa189f431a5cf
luigizuniga <luigialbertozuniga@gmail.com>
2021-07-14 13:06:29 -0400

    jestunit - 03 Implementado pruebas Number Array Boolean

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,11 +1,37 @@
-// First Test With Jest
-// texto a comparar
+const frameworks = ['React','Vue','Angular','React Native'];
 const textCompare = 'I´m Ironman';
 
-// Prueba
+// Reverse strings function
+const reverseString = (str, cb) => {
+    cb(str.split("").reverse().join(""));
+  };
+
+
+// Test String
 test("It should has the word Ironman", () => {
     expect(textCompare).toMatch(/Ironman/)
 });
 
+// Test Array
+test("It Should has react",()=> {
+    expect(frameworks).toContain("React");
+});
 
 
+
+// Test numbers
+test("It should be greater than", ()=>{
+    expect(10).toBeGreaterThan(9);
+});
+
+// Test Booleans
+test("It should be true", () => {
+    expect(true).toBeTruthy();
+});
+
+ // Test callbacks
+test("The callback should return a reversed word", () => {
+    reverseString("Platzi", str => {
+      expect(str).toBe("iztalP");
+    });
+});
